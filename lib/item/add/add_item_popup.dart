cdd51540e3f4a393ec2aa4144024ad8c61ccd412
ptylczynski <piotr@ptl.cloud>
2022-08-26 01:06:55 +0200

    change: add item popup to modify item popup

--- lib/item/add/add_item_popup.dart
+++ lib/item/add/add_item_popup.dart
@@ -3,47 +3,100 @@ import 'package:indexer_client/item/add/barcode_input.dart';
 import 'package:indexer_client/item/add/description_input.dart';
 import 'package:indexer_client/item/add/duedate_input.dart';
 import 'package:indexer_client/item/add/name_input.dart';
+import 'package:indexer_client/item/add/place_input.dart';
 
 import '../../api/api_spec.swagger.dart';
+import '../../common/exceptions/ApiException.dart';
 import '../item_service.dart';
 
-class AddItemPopup extends StatelessWidget {
+class ModifyItemPopup extends StatefulWidget {
+  final bool addNew;
+  final ItemService itemService;
+  final ItemDTO? item;
+
+  ModifyItemPopup(
+      {Key? key, required this.itemService, required this.addNew, this.item})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ModifyItemPopupState(
+      itemService: itemService, addNew: addNew, item: item);
+}
+
+class _ModifyItemPopupState extends State<ModifyItemPopup> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController barcodeController = TextEditingController();
   final TextEditingController dueDateController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey();
   final ItemService itemService;
-  
-  AddItemPopup({Key? key, required this.itemService}) : super(key: key);
+  final bool addNew;
+  PlaceDTO? _selectedPlace;
+  ItemDTO? item;
+
+  _ModifyItemPopupState(
+      {required this.itemService, required this.addNew, this.item});
+
+  @override
+  void initState() {
+    super.initState();
+    if (item != null) {
+      setState(() {
+        nameController.text = item!.name;
+        barcodeController.text = item!.barcode ?? "";
+        dueDateController.text = item!.dueDate!.toIso8601String();
+        descriptionController.text = item!.description ?? "";
+        _selectedPlace = item!.storagePlace;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
       title: const Text("Add Item"),
       children: [
-        Form(
-          key: formKey,
-            child: Column(
-              children: [
-                ItemNameTextInput(
-                    controller: nameController
-                ),
-                ItemDescriptionInput(
-                    controller: descriptionController
-                ),
-                ItemBarcodeInput(
-                    controller: barcodeController
-                ),
-                ItemDueDateInput(
-                    controller: dueDateController
-                ),
-                ElevatedButton(
-                    onPressed: () { onSubmit(context); },
-                    child: const Text("Submit")
-                )
-              ],
-            )
+        Container(
+          margin: const EdgeInsets.all(20),
+          child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: ItemNameTextInput(controller: nameController),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child:
+                        ItemDescriptionInput(controller: descriptionController),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: ItemPlaceInput(
+                      value: _selectedPlace,
+                      onChanged: onPlaceChanged,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: ItemBarcodeInput(controller: barcodeController),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: ItemDueDateInput(controller: dueDateController),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          onSubmit(context);
+                        },
+                        child: const Text("Submit")),
+                  )
+                ],
+              )),
         )
       ],
     );
@@ -52,29 +105,43 @@ class AddItemPopup extends StatelessWidget {
 
   void onSubmit(BuildContext context) {
     if (formKey.currentState!.validate()) {
-      final PlaceDTO place = PlaceDTO(
-        id: 1,
-        name: ""
-      );
+      int id = -1;
+      if (this.item != null) {
+        id = this.item!.id!.toInt();
+      }
       final ItemDTO item = ItemDTO(
-        id: -1,
-        name: nameController.text,
-        barcode: barcodeController.text,
-        dueDate: DateTime.parse(dueDateController.text),
-        description: descriptionController.text,
-        barcodeType: ItemDTOBarcodeType.ean,
-        storagePlace: place
-      );
-      try {
+          id: id,
+          name: nameController.text,
+          barcode: barcodeController.text,
+          dueDate: DateTime.parse(dueDateController.text),
+          description: descriptionController.text,
+          barcodeType: ItemDTOBarcodeType.ean,
+          storagePlace: _selectedPlace);
+      if (addNew) {
         itemService
             .saveItem(item)
-            .then((value) => Navigator.pop(context, value));
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
-        Navigator.pop(context);
+            .then((value) => Navigator.pop(context, value))
+            .onError((error, stackTrace) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text((error as ApiException).reason)));
+        }, test: (o) => o is ApiException);
+      } else {
+        itemService
+            .updateItem(item)
+            .then((value) => Navigator.pop(context, value))
+            .onError((error, stackTrace) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text((error as ApiException).reason)));
+        }, test: (o) => o is ApiException);
       }
-
     }
+  }
 
+  onPlaceChanged(PlaceDTO? place) {
+    if (place != null) {
+      setState(() {
+        _selectedPlace = place;
+      });
+    }
   }
 }
