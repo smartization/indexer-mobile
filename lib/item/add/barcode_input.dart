4445a8ed04d1839ac3a42a1174dc864fb0ca98bd
ptylczynski <piotr@ptl.cloud>
2022-08-27 23:45:13 +0200

    change: add button to scan barcodes near barcode input

--- lib/item/add/barcode_input.dart
+++ lib/item/add/barcode_input.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ItemBarcodeInput extends StatelessWidget {
   final TextEditingController controller;
 
-  const ItemBarcodeInput({Key? key, required this.controller}) : super(key: key);
+  const ItemBarcodeInput({Key? key, required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: const InputDecoration(
-        labelText: "Barcode"
-      ),
-      keyboardType: TextInputType.number,
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: controller,
+            decoration: const InputDecoration(labelText: "Barcode"),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        TextButton(
+            onPressed: onCameraButtonPressed, child: const Icon(Icons.camera))
+      ],
     );
   }
 
+  void onCameraButtonPressed() async {
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+    controller.text = barcodeScanRes;
+  }
 }
