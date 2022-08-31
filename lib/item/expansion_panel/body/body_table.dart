7ca2e9c17c10636c77feb5de6c51e1ec8257bdab
ptylczynski <piotr@ptl.cloud>
2022-08-31 15:41:18 +0200

    add: body_table to item expansion panel body

---
+++ lib/item/expansion_panel/body/body_table.dart
@@ -0,0 +1,56 @@
+part of "body_main.dart";
+
+class _ItemTable extends StatelessWidget {
+  late Map<String, String?> tableValues;
+
+  final ItemDTO item;
+
+  _ItemTable({required this.item}) {
+    tableValues = {
+      "Name": item.name,
+      "Description": item.description,
+      "Storage Place":
+          item.storagePlace == null ? "Not specified" : item.storagePlace!.name,
+      "Due Date": item.dueDate == null
+          ? null
+          : "${item.dueDate!.day}/${item.dueDate!.month}/${item.dueDate!.year}",
+      "Barcode": item.barcode
+    };
+    tableValues.removeWhere((key, value) => value == null || value.isEmpty);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: const TableBorder(
+          bottom: BorderSide(color: Colors.grey),
+          horizontalInside: BorderSide(color: Colors.grey)),
+      children: _generateChildren(),
+    );
+  }
+
+  List<TableRow> _generateChildren() {
+    return tableValues
+        .map((key, value) => MapEntry(key, _createTableRow(key, value)))
+        .values
+        .toList();
+  }
+
+  TableRow _createTableRow(String title, String? value) {
+    return TableRow(children: [
+      Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          height: 3,
+        ),
+      ),
+      Text(
+        value!,
+        style: const TextStyle(
+          height: 3,
+        ),
+      )
+    ]);
+  }
+}
