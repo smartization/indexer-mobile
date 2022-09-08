b3fca02754c245c053267deb43f0dd3d354eca83
ptylczynski <piotr@ptl.cloud>
2022-09-09 01:29:26 +0200

    change: add increment and decrement listeners to items expandable list body

--- lib/item/expansion_panel/body/body_main.dart
+++ lib/item/expansion_panel/body/body_main.dart
@@ -3,19 +3,24 @@ import 'package:flutter/material.dart';
 import '../../../api/api_spec.swagger.dart';
 
 part "body_table.dart";
-
 part "bottom_buttons.dart";
+
+part "quantity_selector.dart";
 
 class ItemExpansionPanelBody extends StatelessWidget {
   final ItemDTO item;
   final Function(ItemDTO) onDelete;
   final Function(ItemDTO) onEdit;
+  final Function(int) onIncrement;
+  final Function(int) onDecrement;
 
   const ItemExpansionPanelBody(
       {Key? key,
       required this.item,
       required this.onDelete,
-      required this.onEdit})
+      required this.onEdit,
+      required this.onIncrement,
+      required this.onDecrement})
       : super(key: key);
 
   @override
@@ -25,6 +30,10 @@ class ItemExpansionPanelBody extends StatelessWidget {
         child: Column(
           children: [
             _ItemTable(item: item),
+            _QuantitySelector(
+                onIncrement: onIncrement,
+                onDecrement: onDecrement,
+                value: item.quantity),
             _BottomButtons(
               onDelete: () => onDelete(item),
               onEdit: () => onEdit(item),
