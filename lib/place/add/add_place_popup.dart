9469a3d6a69066ea55633bef54282e0429945b6e
ptylczynski <piotr@ptl.cloud>
2022-09-12 23:50:04 +0200

    change: add edit option to add place popup

--- lib/place/add/add_place_popup.dart
+++ lib/place/add/add_place_popup.dart
@@ -5,42 +5,84 @@ import '../../api/api_spec.swagger.dart';
 import '../../common/exceptions/exception_resolver.dart';
 import 'name_input.dart';
 
-class AddPlacePopup extends StatelessWidget {
+class AddPlacePopup extends StatefulWidget {
+  final PlaceService placeService;
+  final bool addNew;
+  final ExceptionResolver exceptionResolver;
+  final PlaceDTO? place;
+
+  const AddPlacePopup(
+      {Key? key,
+      required this.placeService,
+      required this.exceptionResolver,
+      required this.addNew,
+      this.place})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AddPlacePopupState(
+      placeService: placeService,
+      exceptionResolver: exceptionResolver,
+      addNew: addNew,
+      place: place);
+}
+
+class _AddPlacePopupState extends State<AddPlacePopup> {
   final TextEditingController nameController = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey();
+  final GlobalKey<FormState> formKey = GlobalKey(debugLabel: "place");
   final PlaceService placeService;
   final ExceptionResolver exceptionResolver;
+  final bool addNew;
+  final PlaceDTO? place;
 
-  AddPlacePopup(
-      {Key? key, required this.placeService, required this.exceptionResolver})
-      : super(key: key);
+  _AddPlacePopupState(
+      {Key? key,
+      required this.placeService,
+      required this.exceptionResolver,
+      required this.addNew,
+      this.place});
 
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
       title: const Text("Add Item"),
       children: [
-        Form(
-            key: formKey,
-            child: Column(
-              children: [
-                PlaceNameTextInput(controller: nameController),
-                ElevatedButton(
-                    onPressed: () {
-                      onSubmit(context);
-                    },
-                    child: const Text("Submit"))
-              ],
-            ))
+        Container(
+          margin: const EdgeInsets.all(20),
+          child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  PlaceNameTextInput(controller: nameController),
+                  ElevatedButton(
+                      onPressed: () {
+                        onSubmit(context);
+                      },
+                      child: const Text("Submit"))
+                ],
+              )),
+        )
       ],
     );
   }
 
   void onSubmit(BuildContext context) {
     if (formKey.currentState!.validate()) {
-      final PlaceDTO place = PlaceDTO(name: nameController.text);
+      num id = -1;
+      if (this.place != null) {
+        id = this.place!.id!;
+      }
+      final PlaceDTO place = PlaceDTO(id: id, name: nameController.text);
       try {
-        placeService.save(place).then((value) => Navigator.pop(context, value));
+        if (addNew) {
+          placeService
+              .save(place)
+              .then((value) => Navigator.pop(context, value));
+        } else {
+          placeService
+              .update(place)
+              .then((value) => Navigator.pop(context, value));
+        }
       } on Exception catch (e) {
         exceptionResolver.resolveAndShow(e);
         Navigator.pop(context);
