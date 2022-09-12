cc23b3047c392dca92862a5ace82578de96a2db7
ptylczynski <piotr@ptl.cloud>
2022-09-12 23:53:31 +0200

    change: add key to drawer_home_button.dart

--- lib/drawer/drawer_home_button.dart
+++ lib/drawer/drawer_home_button.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:indexer_client/item/item_main.dart';
-import 'package:indexer_client/main.dart';
-import 'package:indexer_client/settings/settings_main.dart';
 
 class DrawerHomeButton extends StatelessWidget {
+  const DrawerHomeButton({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("Items"),
-      subtitle: const Text("List of all items"),
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (ctx) => const ItemMain()));
-      }
-    );
+        title: const Text("Items"),
+        subtitle: const Text("List of all items"),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (ctx) => const ItemMain()));
+        });
   }
 
 }
