effc4228cd8af6cadcaa4e99a1bc7db1582688a9
ptylczynski <piotr@ptl.cloud>
2022-09-12 23:53:40 +0200

    change: add key to drawer_settings_button.dart

--- lib/drawer/drawer_settings_button.dart
+++ lib/drawer/drawer_settings_button.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 import '../settings/settings_main.dart';
 
 class DrawerSettingsButton extends StatelessWidget {
+  const DrawerSettingsButton({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("Settings"),
-      subtitle: const Text("Application settings"),
-      onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (ctx) => SettingsMain()));
-      }
-    );
+        title: const Text("Settings"),
+        subtitle: const Text("Application settings"),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (ctx) => SettingsMain()));
+        });
   }
 
 }
