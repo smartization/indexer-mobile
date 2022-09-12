1721057549be7308fe7caa2c5bc38b210ea90c5c
ptylczynski <piotr@ptl.cloud>
2022-09-12 23:50:29 +0200

    add: body_main for categories

---
+++ lib/category/expansion_panel/body/body_main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:indexer_client/common/loading_indicator.dart';
+import 'package:indexer_client/item/barcode_service.dart';
+import 'package:indexer_client/item/item_service.dart';
+
+import '../../../api/api_spec.swagger.dart';
+import '../../../common/exceptions/exception_resolver.dart';
+import '../../../item/add/add_item_popup.dart';
+import '../../../item/item_expansion_list.dart';
+
+part 'bottom_buttons.dart';
+
+part "category_table.dart";
+
+part 'items_list.dart';
+
+class CategoryExpansionPanelBody extends StatelessWidget {
+  final CategoryDTO category;
+  final Function(CategoryDTO) onDelete;
+  final Function(CategoryDTO) onEdit;
+
+  const CategoryExpansionPanelBody(
+      {Key? key,
+      required this.category,
+      required this.onDelete,
+      required this.onEdit})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        child: Column(
+          children: [
+            _CategoryTable(category: category),
+            _ItemsList(category: category),
+            _BottomButtons(
+              onDelete: () => onDelete(category),
+              onEdit: () => onEdit(category),
+            )
+          ],
+        ));
+  }
+}
