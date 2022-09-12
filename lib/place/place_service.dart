2e105ac73f73ea553c85281a81644be9e72ac74b
ptylczynski <piotr@ptl.cloud>
2022-09-12 23:56:07 +0200

    change: add update to place_service.dart

--- lib/place/place_service.dart
+++ lib/place/place_service.dart
@@ -1,9 +1,15 @@
 import 'package:chopper/chopper.dart';
+import 'package:flutter/material.dart';
 import 'package:indexer_client/api/api_spec.swagger.dart';
 import 'package:indexer_client/common/dto_service.dart';
 
+import '../common/exceptions/ApiException.dart';
+import '../common/exceptions/exception_resolver.dart';
+
 class PlaceService extends DTOService {
-  PlaceService({required super.context});
+  final ExceptionResolver exceptionResolver;
+
+  PlaceService({required super.context, required this.exceptionResolver});
 
   Future<List<PlaceDTO>> getAll() async {
     Response<List<PlaceDTO>> response = await getApi().placesGet();
@@ -15,9 +21,36 @@ class PlaceService extends DTOService {
     return resolveResponse(response) as PlaceDTO;
   }
 
+  Future<PlaceDTO> update(PlaceDTO place) async {
+    Response<PlaceDTO> response = await getApi().placesPut(body: place);
+    return resolveResponse(response) as PlaceDTO;
+  }
+
   Future<PlaceDTO> delete(PlaceDTO place) async {
     Response<dynamic> response = await getApi().placesIdDelete(id: place.id);
     resolveResponse(response);
     return Future.value();
+  }
+
+  onPlaceEditedListener(
+      Future<PlaceDTO?> editedPlace, PlaceDTO oldPlace, List<PlaceDTO> places,
+      {showSaveNotification = true}) async {
+    PlaceDTO? place = await editedPlace;
+    try {
+      if (place != null) {
+        int idx = places.indexOf(oldPlace);
+        places.removeAt(idx);
+        places.insert(idx, place);
+        if (showSaveNotification) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("Saving")));
+        }
+      } else if (showSaveNotification) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Item was not changed")));
+      }
+    } on ApiException catch (error) {
+      exceptionResolver.resolveAndShow(error);
+    }
   }
 }
