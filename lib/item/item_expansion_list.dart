09e1bc7afcad3584a2e4f2649949bb1b41871e36
ptylczynski <piotr@ptl.cloud>
2022-11-21 00:21:10 +0100

    change: add state to item_expansion_list.dartin item

--- lib/item/item_expansion_list.dart
+++ lib/item/item_expansion_list.dart
@@ -1,66 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:indexer_client/state.dart';
+import 'package:provider/provider.dart';
 
 import '../api/api_spec.swagger.dart';
 import 'expansion_panel/item_expansion_panel.dart';
+import 'item_service.dart';
 
 class ItemExpansionList extends StatelessWidget {
   final List<ItemDTO> items;
-  final Function(ItemDTO, bool)? onExpanded;
-  final Function(ItemDTO) onItemDelete;
-  final Function(ItemDTO) onItemEdited;
-  final Map<ItemDTO, bool> expanded;
-  final Function(int?, ItemDTO) onIncrement;
-  final Function(int?, ItemDTO) onDecrement;
-  final Future<void> Function()? onRefresh;
   final bool refreshable;
 
   const ItemExpansionList(
-      {Key? key,
-      required this.items,
-      this.onExpanded,
-      expandedList,
-      required this.onItemDelete,
-      required this.onItemEdited,
-      required this.onIncrement,
-      required this.onDecrement,
-      this.onRefresh,
-      this.refreshable = false,
-      required this.expanded})
+      {Key? key, required this.items, expandedList, this.refreshable = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (!refreshable) {
-      return getList();
+      return getList(context);
     } else {
       return RefreshIndicator(
-        onRefresh: onRefresh ?? () async {},
-        child: getList(),
+        onRefresh: () => _onRefresh(context),
+        child: getList(context),
       );
     }
   }
 
-  List<ExpansionPanel> generateChildren() {
-    return items
-        .map((e) => ItemExpansionPanel.from(e, expanded[e]!, onItemDelete,
-            onItemEdited, onIncrement, onDecrement))
-        .toList();
+  List<ExpansionPanel> generateChildren(BuildContext context) {
+    return items.map((e) => ItemExpansionPanel.from(e, context)).toList();
   }
 
-  getList() {
+  getList(BuildContext context) {
     return SingleChildScrollView(
       physics: const AlwaysScrollableScrollPhysics(),
       child: ExpansionPanelList(
-        children: generateChildren(),
-        expansionCallback: _onExpanded,
+        children: generateChildren(context),
+        expansionCallback: (idx, state) => _onExpanded(idx, state, context),
       ),
     );
   }
 
-  void _onExpanded(int panelIndex, bool isExpanded) {
+  void _onExpanded(int panelIndex, bool isExpanded, BuildContext context) {
     ItemDTO item = items.elementAt(panelIndex);
-    if (onExpanded != null) {
-      onExpanded!(item, isExpanded);
-    }
+    Provider.of<AppState>(context, listen: false).expandItem(item);
+  }
+
+  _onRefresh(BuildContext context) {
+    Future<List<ItemDTO>> items =
+        ItemService.getInstance(context).getAllItems();
+    items.then((value) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Reloaded")));
+      Provider.of<AppState>(context).updateItems(value);
+    });
   }
 }
