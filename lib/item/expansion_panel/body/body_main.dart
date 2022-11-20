90b0258b77ea7535273eee52020764b4431ddad0
ptylczynski <piotr@ptl.cloud>
2022-11-21 00:19:02 +0100

    change: add state to item expansion panel

--- lib/item/expansion_panel/body/body_main.dart
+++ lib/item/expansion_panel/body/body_main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:indexer_client/common/exceptions/exception_resolver.dart';
 import 'package:indexer_client/common/item_warning_resolver.dart';
+import 'package:indexer_client/item/barcode_service.dart';
+import 'package:indexer_client/item/item_service.dart';
 
 import '../../../api/api_spec.swagger.dart';
+import '../../add/add_item_popup.dart';
 
 part "body_table.dart";
 
@@ -13,18 +17,8 @@ part "quantity_selector.dart";
 
 class ItemExpansionPanelBody extends StatelessWidget {
   final ItemDTO item;
-  final Function(ItemDTO) onDelete;
-  final Function(ItemDTO) onEdit;
-  final Function(int) onIncrement;
-  final Function(int) onDecrement;
 
-  const ItemExpansionPanelBody(
-      {Key? key,
-      required this.item,
-      required this.onDelete,
-      required this.onEdit,
-      required this.onIncrement,
-      required this.onDecrement})
+  const ItemExpansionPanelBody({Key? key, required this.item})
       : super(key: key);
 
   @override
@@ -36,15 +30,36 @@ class ItemExpansionPanelBody extends StatelessWidget {
             _ItemWarningText(item: item),
             _ItemTable(item: item),
             _QuantitySelector(
-                onIncrement: onIncrement,
-                onDecrement: onDecrement,
+                onIncrement: (qty) => _onQuantityChange(context, qty),
+                onDecrement: (qty) => _onQuantityChange(context, qty),
                 value: item.quantity),
             _BottomButtons(
-              onDelete: () => onDelete(item),
-              onEdit: () => onEdit(item),
+              onDelete: () => _onDelete(context),
+              onEdit: () => _onEdit(context),
             )
           ],
-        )
-    );
+        ));
+  }
+
+  void _onDelete(BuildContext context) {
+    ItemService.getInstance(context).itemDeleteListener(item);
+  }
+
+  _onEdit(BuildContext context) {
+    Future<ItemDTO?> editedItem = showDialog<ItemDTO>(
+        context: context,
+        builder: (ctx) => ModifyItemPopup(
+              itemService: ItemService.getInstance(context),
+              barcodeService: BarcodeService.getInstance(context),
+              exceptionResolver: ExceptionResolver.getInstance(context),
+              addNew: false,
+              item: item,
+            ));
+    ItemService.getInstance(context).onItemEditedListener(editedItem, item);
+  }
+
+  _onQuantityChange(BuildContext context, int quantity) {
+    ItemDTO newItem = $ItemDTOExtension(item).copyWith(quantity: quantity);
+    ItemService.getInstance(context).saveAndUpdateList(newItem, item);
   }
 }
